c732d55f84e810761326b13d0c4b007aef381d6b
nofirst <1025926969@qq.com>
2026-01-02 16:59:36 +0800

    init commit

---
+++ src/main/java/com/nofirst/spring/tdd/demo/SpringTddDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nofirst.spring.tdd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTddDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTddDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nofirst/spring/tdd/demo/SpringTddDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nofirst.spring.tdd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+@SpringBootTest
+class SpringTddDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
